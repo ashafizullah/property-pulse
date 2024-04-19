173cd35bbc156a5ef73727714df947eb3a4d3cf8
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-04-20 06:47:54 +0700

    update: add form action to property add

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -14,3 +14,49 @@ export const GET = async (request) => {
     return new Response('Something went wrong', {status: 500});
   }
 }
+
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+
+    //Access all values from amenities and images
+    const amenities = formData.getAll('amenities');
+    const images = formData.getAll('images').filter((images) => images.name !== '');
+
+    //create propertyData object for database
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode')
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        nightly: formData.get('rates.nightly'),
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly')
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        phone: formData.get('seller_info.phone'),
+        email: formData.get('seller_info.email')
+      },
+      images
+    }
+
+    console.log(propertyData)
+
+    return new Response(JSON.stringify({
+      message: 'Success'
+    }, {status: 200}));
+  } catch (error) {
+    return new Response('Failed to add prooperty', { status: 500 });
+  }
+}
